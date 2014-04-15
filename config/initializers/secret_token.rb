05e2973e92049008266648c952d4223dadca3c3c
Kavita Jadhav <kavitama@INkavitama.local>
2014-04-15 17:24:13 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnixToolsInRuby::Application.config.secret_key_base = '04ad0542c40c873c4009938de54dc97bd5d479ed372c63e226f0b68011eb2fbc59b85bbf018e3edbc14a50694f4caaca72c7ff54fc89c258f80396869cf1aeda'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UnixToolsInRuby::Application.config.session_store :cookie_store, key: '_unix-tools-in-ruby_session'
