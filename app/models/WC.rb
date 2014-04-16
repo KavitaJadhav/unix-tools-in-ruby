c2f577541d9ec02ab4dd51fdbfc9c69a9da0464e
Kavita Jadhav <kavitama@INkavitama.local>
2014-04-16 09:53:18 +0530

    Read and writing text file

--- app/models/WC.rb
+++ app/models/WC.rb
@@ -23,4 +23,15 @@ class WC
   def print_count data, options
     puts get_count data, options
   end
+
+  def read_from_file file_name
+    file = File.open(file_name, "r")
+    file_data = file.read
+    file.close
+    file_data
+  end
+
+  def write_to_file file_name, text
+    File.open(file_name, 'w') {|f| f.write(text) }
+  end
 end
